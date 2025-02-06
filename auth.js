923ab5de7fe9a5209bccb8b343b3df257fca43d1
tyagi077 <tyagimayank0501@gmail.com>
2025-02-06 19:34:23 +0530

    bug fix

--- auth.js
+++ auth.js
@@ -2,7 +2,7 @@ const jwt = require("jsonwebtoken");
 const SECRET_KEY = "s3cret";
 
 function auth(req, res, next) {
-    const token = req.headers.authorization;
+    const token = req.headers.token;
 
     const response = jwt.verify(token, SECRET_KEY);
 
